e77ef22fb59d29f3af52f0235c28aed829b533ad
Camilozu <69897712+Camilozu@users.noreply.github.com>
2020-10-04 00:19:44 -0500

    Create Vehiculo.java
    Clase vehiculo

---
+++ src/Vehiculo1/Vehiculo.java
@@ -0,0 +1,74 @@
+package Vehiculo1;
+import gestionHumana.*;
+
+public class Vehiculo {
+	private String placa;
+	//private String propietario;
+	private int matricula;
+	private String marca;
+	private Boolean documentosAlDia;
+	private Repartidor repartidor;
+		
+	
+	Vehiculo(String placa, int matricula, String marca, Repartidor repartidor) {
+		this.placa = placa;
+		this.matricula = matricula;
+		this.marca = marca;
+		this.repartidor = repartidor;
+		this.estadoDocumentos();
+	}
+	
+	public Boolean estadoDocumentos() {
+		if  (placa.equals(null) || matricula == 0 || marca.equals(null) )  {
+			
+			return documentosAlDia = false;
+		}
+		else {
+			return documentosAlDia = true;
+		}
+		
+	}
+	
+				
+	public String getPlaca() {
+		return placa;
+	}
+	public void setPlaca(String placa) {
+		this.placa = placa;
+	}
+	/*public String getPropietario() {
+		return propietario;
+	}
+	public void setPropietario(String propietario) {
+		this.propietario = propietario;
+	}*/
+	public int getMatricula() {
+		return matricula;
+	}
+	public void setMatricula(int matricula) {
+		this.matricula = matricula;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public Boolean getDocumentosAldia() {
+		return documentosAlDia;
+	}
+	public void setDocumentosAldia(Boolean documentosAldia) {
+		this.documentosAlDia = documentosAldia;
+	}
+	public Repartidor getRepartidor() {
+		return repartidor;
+	}
+	public void setRepartidor(Repartidor repartidor) {
+		this.repartidor = repartidor;
+	}
+	
+	
+
+}
+
+
