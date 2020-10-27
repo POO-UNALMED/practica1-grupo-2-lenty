3eef97ac7e56c16bf50ad300e43110ee25fe4e8e
Camilozu <69897712+Camilozu@users.noreply.github.com>
2020-10-27 16:38:16 -0500

    Sedes

--- src/interfaz/Main.java
+++ src/interfaz/Main.java
@@ -138,39 +138,43 @@ public class Main {
 				op=in.next();
 				switch(op) {
 				
-				case"1":
-					System.out.println("Ingrese la placa del vehiculo (sin espacios)");
-					String placa1 = in.next();
-					vehiculo1.setPlaca(placa1);
-					break;
-				case"2":
-					System.out.println("Ingrese el modelo del vehiculo (sin espacios)");
-					String modelo1 = in.next();
-					vehiculo1.setModelo(modelo1);
-					break;
-				case"3":
-					System.out.println("Ingrese la placa del vehiculo (Solo numeros)");
-					int matricula1 = in.nextInt();
-					vehiculo1.setMatricula(matricula1);
-					break;    
-				case"4":
-					System.out.println("Ingrese true si el vehiculo se encuentra aegurado o false en caso contrario");
-					boolean asegurado1 = in.nextBoolean();
-					vehiculo1.setAsegurado(asegurado1);
-					break;
-				case"5":
-					menuVehiculos();
-					break; 
-				//default:
-					//System.out.println("Ingresa una opcion valida");
-					
+				    case"1":
+				    	
+					    System.out.println("Ingrese la placa del vehiculo (sin espacios)");
+					    String placa1 = in.next();
+					    vehiculo1.setPlaca(placa1);
+					    menuVehiculos();
+					    break;
+				    case"2":
+					    System.out.println("Ingrese el modelo del vehiculo (sin espacios)");
+					    String modelo1 = in.next();
+					    vehiculo1.setModelo(modelo1);
+					    menuVehiculos();
+					    break;
+				    case"3":
+					    System.out.println("Ingrese la placa del vehiculo (Solo numeros)");
+					    int matricula1 = in.nextInt();
+					    vehiculo1.setMatricula(matricula1);
+					    menuVehiculos();
+					    break;    
+				    case"4":
+					    System.out.println("Ingrese true si el vehiculo se encuentra aegurado o false en caso contrario");
+					    boolean asegurado1 = in.nextBoolean();
+					    vehiculo1.setAsegurado(asegurado1);
+					    menuVehiculos();
+					    break;
+				    case"5":
+					    menuVehiculos();
+					    break; 
+				
 				}
 				break;
 			case "4":
 				Vehiculo.consultarVehiculo();
-				System.out.println("Ingrese el numero de la lista del vehiculo que desea modificar");
+				System.out.println("Ingrese el numero de la lista del vehiculo que desea Eliminar");
 				int numero1 = in.nextInt();
 				(Vehiculo.getVehiculos()).remove(numero1);
+				menuVehiculos();
 				break;
 				
 			case "5":
@@ -186,11 +190,11 @@ public class Main {
 		System.out.println("________________________\n");
 		System.out.println("Menú Usuarios");
 		System.out.println("Seleccione una opcion");
-		String aux="1 - Clientes \n2 - Repartidores\n3 - Regresar";
+		String aux= "1 - Consultar sede \n2 - Registrar sede \n3 - Modificar sede \n4 - Eliminar sede \n5 - Contultar sede de mayor ventas  \n6 Regresar al menu principal";
 		System.out.println(aux);
 		while (true) {
 			op=in.next();
-			if(op.equals("1") || op.equals("2") || op.equals("3")) {
+			if(op.equals("1") || op.equals("2") || op.equals("3") || op.equals("4") || op.equals("5") || op.equals("6")) {
 				break;
 			}
 			else {
@@ -200,16 +204,60 @@ public class Main {
 		}
 		switch(op) {
 			case "1":
-				
+				Sede.consultarSedes();
+				menuSedes();
 				break;
 				
 			case "2":
-				System.out.println("Se entro 2");
+				System.out.println("Ingrese la direccion(Sin espacios)"); 
+				String dir = in.next();
+				System.out.println("Ingrese el numero telefonico(Sin espacios)");
+				int telefono = in.nextInt();
+				Sede sede = new Sede (dir, telefono);
+				Sede.adicionarSede(sede);
 				break;
 				
 			case "3":
-				menuInicial();
+				Sede.consultarSedes();
+				System.out.println("Ingrese el numero de la lista de la sede que desea modificar");
+				int numero = in.nextInt();
+				Sede sede1 =(Sede.getSede()).get(numero);
+				System.out.println("Seleccione la caracteristica a modificar");
+				System.out.println("1 - Direccion \n2 - Telefono \n3 -Regresar al menu sede");
+				op=in.next();
+				
+				switch(op) {
+				
+			    case"1":
+			    	
+				    System.out.println("Ingrese la nueva direccion (sin espacios)");
+				    String dir1 = in.next();
+				    sede1.setDireccion(dir1);
+				    menuSedes();
+				    break;
+			    case"2":
+			    	 System.out.println("Ingrese el nuevo numero telefonico (sin espacios)");
+					    long dir11 = in.nextLong();
+					    sede1.setTelefono(dir11);
+					    menuSedes();
+			    case"3":
+				     	menuSedes();
 				break;
+				}
+			case "4":
+				Sede.consultarSedes();
+				System.out.println("Ingrese el numero de la lista de la sede que desea Eliminar");
+				int numero1 = in.nextInt();
+				(Sede.getSede()).remove(numero1);
+				menuSedes();
+				break;
+			case "5":
+				Sede.sedeMayorVentas();
+				menuSedes();
+				break;
+			case "6":
+				menuInicial();
+				
 
 		}
 	}
