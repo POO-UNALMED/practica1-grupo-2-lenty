720147c467adeceebcfc3cd3db6882cdc5995ed3
Danilo <danilogiraldo33@gmail.com>
2020-10-27 00:45:21 -0500

    Metodos Orden

--- src/interfaz/Main.java
+++ src/interfaz/Main.java
@@ -2,6 +2,8 @@ package interfaz;
 
 import java.util.*;
 
+import baseDatos.Escritor;
+import baseDatos.Lector;
 import gestionHumana.*;
 import orden.*;
 import sede.*;
@@ -12,8 +14,8 @@ public class Main {
 	static String op;
 	
 	public static void main(String[] args) {		
-		System.out.println("Bienvenido");				
-		
+						
+		Lector.Leer();
 		menuInicial();
 				
 	}
@@ -21,11 +23,11 @@ public class Main {
 	static void menuInicial () {
 		System.out.println("________________________\n");
 		System.out.println("Seleccione una opcion");
-		String aux="1 - Menú de Usuarios \n2 - Menú de Ordenes\n3 - Menú de Productos \n4 - Menú de Vehiculos \n5 - Menú de Sedes";
+		String aux="1 - Menú de Usuarios \n2 - Menú de Ordenes\n3 - Menú de Productos \n4 - Menú de Vehiculos \n5 - Menú de Sedes \n6 - Guardar cambios y salir";
 		System.out.println(aux);
 		while (true) {
 			op=in.next();
-			if(op.equals("1") || op.equals("2") || op.equals("3") || op.equals("4") || op.equals("5")) {
+			if(op.equals("1") || op.equals("2") || op.equals("3") || op.equals("4") || op.equals("5") || op.equals("6")) {
 				break;
 			}
 			else {
@@ -39,26 +41,360 @@ public class Main {
 				break;
 				
 			case "2":
-				System.out.println("Se entro 2");
+				menuOrden();
 				break;
 				
 			case "3":
-				System.out.println("Se entro 3");
+				menuProductos();
 				break;
 				
 			case "4":
-				System.out.println("Se entro 4");
+				menuVehiculos();
 				break;
 				
 			case "5":
-				System.out.println("Se entro 5");
+				menuSedes();
+				break;
+			case "6":
+				Escritor.Escribir();;
 				break;
 		}
 	}
-	
+	public static void menuOrden() {
+		System.out.println("________________________\n");
+		System.out.println("Menú Orden");
+		System.out.println("Seleccione una opcion");
+		String aux="1 - Crear orden \n2 - Modificar orden\n3 - Eliminar orden \n4 - Regresar";
+		System.out.println(aux);
+		while (true) {
+		op=in.next();
+			if(op.equals("1") || op.equals("2") || op.equals("3") || op.equals("4")) {
+				break;
+			}
+			else {
+				System.out.println("Ingresa una opcion valida");
+				System.out.println(aux);	
+			}
+		}
+		switch(op) {
+		case "1":
+			int id;
+			Cliente c;
+			Sede s;
+			Repartidor r;
+			int valor;
+			int cantP;
+			float peso;
+			String es;
+			System.out.println("ingrese los parametros de la orden: \n");
+			System.out.println("ID de la orden:");
+			id=in.nextInt();
+			System.out.println("El cliente de la orden: ");
+			//FALTA CLIENTE, SEDE Y REPARTIDOR
+			System.out.println("Sede de la orden: ");
+			System.out.println("Repartidor de la orden: ");
+			System.out.println("Valor de la Orden: ");
+			valor=in.nextInt();
+			System.out.println("Cantidad de productos: ");
+			cantP=in.nextInt();
+			System.out.println("Peso total de la orden: ");
+			peso=in.nextFloat();
+			System.out.println("Estado de la orden: ");
+			es=in.next();
+			new Orden(id,null,null,null,valor,cantP,peso,es);
+			break;
+		
+		case "2":
+			Orden i=new Orden(1, null, null, null, 10, 2, 15, "Aceptada");
+			//ESCOGER LA ORDEN A MODIFICAR
+			i.modificarOrden();
+			break;
+		
+		case "3":
+			Orden j=new Orden(1, null, null, null, 10, 2, 15, "Aceptada");
+			//ESCOGER LA ORDEN
+			j.cancelarOrden(j);
+			break;
+		
+		case "4":
+			menuInicial();
+			break;
+		}
+	}
 	static void menuUsuarios() {
 		System.out.println("________________________\n");
 		System.out.println("Menú Usuarios");
+		System.out.println("Seleccione una opcion");
+		String aux="1 - Clientes \n2 - Repartidores\n3 - Regresar";
+		System.out.println(aux);
+		while (true) {
+			op=in.next();
+			if(op.equals("1") || op.equals("2") || op.equals("3")) {
+				break;
+			}
+			else {
+				System.out.println("Ingresa una opcion valida");
+				System.out.println(aux);	
+			}
+		}
+		switch(op) {
+			case "1":
+				menuClientes();
+				break;
+				
+			case "2":
+				menuRepartidores();
+				break;
+				
+			case "3":
+				menuInicial();
+				break;
+
+		}
+	}
+	
+	static void menuClientes() {
+		System.out.println("________________________\n");
+		System.out.println("Menú Clientes");
+		System.out.println("Seleccione una opcion");
+		String aux="1 - Ver Clientes \n2 - Agregar Cliente\n3 - Regresar";
+		System.out.println(aux);
+		while (true) {
+			op=in.next();
+			if(op.equals("1") || op.equals("2") || op.equals("3")) {
+				break;
+			}
+			else {
+				System.out.println("Ingresa una opcion valida");
+				System.out.println(aux);	
+			}
+		}
+		switch(op) {
+			case "1":
+				Cliente.verClientes();
+				menuClientes();
+				break;
+				
+			case "2":
+				System.out.print("\nIngrese el numero de documento del Cliente: ");
+				long id = in.nextLong();
+				System.out.print("\nIngrese el nombre del Cliente: ");
+				String nombre = in.next();
+				System.out.print("\nIngrese el telefono del Cliente: ");
+				String telefono = in.next();
+				System.out.print("\nIngrese el genero del Cliente: ");
+				String genero = in.next();
+				System.out.print("\nIngrese la direccion del Cliente: ");
+				String dir = in.next();
+				System.out.print("\nIngrese el metodo de pago del Cliente: \n1 - Efectivo \n2 - Tarjeta");
+				String pago = in.next();
+				String metPago;
+				long tarj;
+				if (pago.equals("1")) {
+					metPago = "Efectivo";
+					tarj = 0000000000;
+				} else {
+					metPago = "Tarjeta";
+					System.out.print("\nIngrese el numero de tarjeta del Cliente: ");
+					tarj = in.nextLong();
+				}
+				Cliente.agregarCliente(dir, metPago, tarj, id, nombre, genero, telefono);
+				menuClientes();
+				break;
+				
+			case "3":
+				menuUsuarios();
+				break;
+
+		}
+	}
+	
+	static void menuRepartidores() {
+		System.out.println("________________________\n");
+		System.out.println("Menú Repartidores");
+		System.out.println("Seleccione una opcion");
+		String aux="1 - Ver Repartidores \n2 - Agregar Repartidor\n3 - Regresar";
+		System.out.println(aux);
+		while (true) {
+			op=in.next();
+			if(op.equals("1") || op.equals("2") || op.equals("3")) {
+				break;
+			}
+			else {
+				System.out.println("Ingresa una opcion valida");
+				System.out.println(aux);	
+			}
+		}
+		switch(op) {
+			case "1":
+				Cliente.verClientes();
+				menuClientes();
+				break;
+				
+			case "2":
+				System.out.print("\nIngrese el numero de documento del Repartidor: ");
+				long id = in.nextLong();
+				System.out.print("\nIngrese el nombre del Repartidor: ");
+				String nombre = in.next();
+				System.out.print("\nIngrese el telefono del Repartidor: ");
+				String telefono = in.next();
+				System.out.print("\nIngrese el genero del Repartidor: ");
+				String genero = in.next();
+				System.out.print("\nIngrese la entidad de salud del Repartidor: ");
+				String entSalud = in.next();
+				System.out.print("\nIngrese el salario del Repartidor: ");
+				int salario = in.nextInt();
+				Repartidor.agregarRepartidor(entSalud, salario, true, id, nombre, genero, telefono);
+				menuRepartidores();
+				break;
+				
+			case "3":
+				menuUsuarios();
+				break;
+
+		}
+	}
+	static void menuProductos() {
+		System.out.println("________________________\n");
+		System.out.println("Menú Productos\n");
+		System.out.println("Seleccione una opcion");
+		String aux="1 - Agregar producto \n2 - Consultar productos en existencia \n3 - Consultar productos mas vendidos \n4 - Regresar";
+		System.out.println(aux);
+		System.out.print("\nIngrese la opcion que desea: ");
+		while (true) {
+			
+			op=in.next();
+			if(op.equals("1") || op.equals("2") || op.equals("3") || op.equals("4")) {
+				break;
+			}
+			else {
+				System.out.print("\nIngresa una opcion valida: ");
+			}
+		}
+		switch(op) {
+		case "1":
+			System.out.print("\nIngrese el nombre del producto: ");
+			String nombre = in.next();
+			System.out.print("\nIngrese la descripcion del producto: ");
+			String descripcion = in.next();
+			System.out.print("\nIngrese el precio del producto: ");
+			long precio = in.nextLong();
+			System.out.print("\nIngrese la cantidad del producto: ");
+			int cantidad = in.nextInt();
+			Producto.agregarProducto(nombre, descripcion, precio, cantidad);
+			menuProductos();
+			break;
+			
+		case "2":
+			Producto.verProductos();
+			menuProductos();
+			break;
+			
+		case "3":
+			System.out.println(Producto.productoMasVendido());
+			menuProductos();
+			break;
+
+		case "4":
+			menuInicial();
+			break;
+	}
+	
+}
+
+	static void menuVehiculos() {
+		System.out.println("________________________\n");
+		System.out.println("Menú Vehiculos");
+		System.out.println("Seleccione una opcion");
+		String aux="1 - Consultar Vehiculos \n2 - Registrar Vehiculo \n3 - Modificar Vehiculo \n4 - Eliminar Vehiculo \n5 - Regresar al menu principal";
+		System.out.println(aux);
+		while (true) {
+			op=in.next();
+			if(op.equals("1") || op.equals("2") || op.equals("3") || op.equals("4") || op.equals("5")) {
+				break;
+			}
+			else {
+				System.out.println("Ingresa una opcion valida");
+				System.out.println(aux);	
+			}
+		}
+		switch(op) {
+			case "1":
+				Vehiculo.consultarVehiculo();
+				menuVehiculos();
+				break;
+				
+			case "2":
+				System.out.println("Ingrese la placa del vehiculo"); 
+				String placa = in.next();
+				System.out.println("Ingrese el modelo del vehiculo(Sin espacios)");
+				String modelo = in.next();
+				System.out.println("Ingrese la matricula (Solo numeros)");
+				int matricula = in.nextInt();
+				System.out.println("Ingrese true si el vehiculo se encuentra aegurado o false en caso contrario");
+				boolean asegurado = in.nextBoolean();
+				Vehiculo vehiculo = new Vehiculo(placa, modelo, matricula, asegurado);
+				Vehiculo.adicionarVehiculo(vehiculo);
+				menuVehiculos();
+				break;
+				
+			case "3":
+				Vehiculo.consultarVehiculo();
+				System.out.println("Ingrese el numero de la lista del vehiculo que desea modificar");
+				int numero = in.nextInt();
+				Vehiculo vehiculo1 =(Vehiculo.getVehiculos()).get(numero);
+				System.out.println("Seleccione la caracteristica a modificar");
+				System.out.println("1 - Placa \n2 - Modelo \n3 - Matricula \n4 - Seguro \n5 -Regresar al menu vehiculo");
+				op=in.next();
+				switch(op) {
+				
+				case"1":
+					System.out.println("Ingrese la placa del vehiculo (sin espacios)");
+					String placa1 = in.next();
+					vehiculo1.setPlaca(placa1);
+					break;
+				case"2":
+					System.out.println("Ingrese el modelo del vehiculo (sin espacios)");
+					String modelo1 = in.next();
+					vehiculo1.setModelo(modelo1);
+					break;
+				case"3":
+					System.out.println("Ingrese la placa del vehiculo (Solo numeros)");
+					int matricula1 = in.nextInt();
+					vehiculo1.setMatricula(matricula1);
+					break;    
+				case"4":
+					System.out.println("Ingrese true si el vehiculo se encuentra aegurado o false en caso contrario");
+					boolean asegurado1 = in.nextBoolean();
+					vehiculo1.setAsegurado(asegurado1);
+					break;
+				case"5":
+					menuVehiculos();
+					break; 
+				//default:
+					//System.out.println("Ingresa una opcion valida");
+					
+				}
+				break;
+			case "4":
+				Vehiculo.consultarVehiculo();
+				System.out.println("Ingrese el numero de la lista del vehiculo que desea modificar");
+				int numero1 = in.nextInt();
+				(Vehiculo.getVehiculos()).remove(numero1);
+				break;
+				
+			case "5":
+				menuInicial();
+				break;	
+
+		}
+	}
+	
+	
+
+	static void menuSedes() {
+		System.out.println("________________________\n");
+		System.out.println("Menú Sedes");
 		System.out.println("Seleccione una opcion");
 		String aux="1 - Clientes \n2 - Repartidores\n3 - Regresar";
 		System.out.println(aux);
@@ -87,5 +423,4 @@ public class Main {
 
 		}
 	}
-	
 }
--- src/orden/Orden.java
+++ src/orden/Orden.java
@@ -1,7 +1,9 @@
 package orden;
 import java.util.*;
+import interfaz.Main;
 import sede.*;
 import gestionHumana.*;
+import interfaz.Main;
 public class Orden {
 	private int id;
 	private Cliente cliente;
@@ -14,7 +16,17 @@ public class Orden {
 	// private List<Producto> productos= new ArrayList<>();
 	private float pesoTotal;
 	public String estado;
-	public static void cancelarOrden(Orden i) {
+	public Orden(int id,Cliente c,Sede s,Repartidor r,int valor,int cantP,float peso,String es) {
+		this.id=id;
+		this.cliente=c;
+		this.sede=s;
+		this.repartidor=r;
+		this.valor=valor;
+		this.cantProductos=cantP;
+		this.pesoTotal=peso;
+		this.estado=es;
+	}
+	public void cancelarOrden(Orden i) {
 		if(i.estado != "Entregada" && i.estado != "En camino") {
 			i = null;
 			System.out.println("Orden cancelada con exito");
@@ -27,15 +39,109 @@ public class Orden {
 		}
 		
 	}
-	public void modificarOrden(Orden i) {
-		
+	public void modificarOrden() {
+		System.out.println("________________________\n");
+		Scanner in=new Scanner(System.in);
+		String op,ch;
+		System.out.println("¿Qué desea modificar?");
+		System.out.println("Seleccione una opcion: ");
+		String aux="\n1 - ID de orden \n2 - Cliente de orden \n3 - Sede de orden \n4 - Repartidor de orden \n5 - Valor de orden \n6 - Cantidad de productos de orden \n7 - Peso total de orden \n8 - Estado de la Orden \n9 - Regresar";
+		System.out.println(aux);
+		while (true) {
+			op=in.next();
+			if(op.equals("1") || op.equals("2") || op.equals("3") || op.equals("4") || op.equals("5") || op.equals("6") || op.equals("7") || op.equals("8") || op.equals("9")) {
+				break;
+			}
+			else {
+				System.out.println("Ingresa una opcion valida");
+				System.out.println(aux);	
+			}
+		}
+		switch(op) {
+		case "1":
+			System.out.println("Ingrese un nuevo ID de la orden :");
+			aux="Ingrese un nuevo ID de la orden :";
+			while(true) {
+				ch=in.next();
+				if(isNumeric(ch)){
+					this.setId(Integer.parseInt(ch));
+					System.out.print("Cambio realizado, siga modificando \n");
+					modificarOrden();
+				}
+				else {
+					System.out.println("Ingresa una opcion valida");
+					System.out.println(aux);
+				}
+			}
+		case "2":
+			//Seleccionar un nuevo cliente para la orden, luego cambiar el anterior por este.
+			break;
+			
+		case "3":
+			//Seleccionar una nueva Sede para la orden
+			break;
+			
+		case "4":
+			//Asignar un nuevo repartidor a la orden
+			break;
+			
+		case "5":
+			System.out.println("Ingrese un nuevo valor de la orden :");
+			aux="Ingrese un nuevo valor de la orden :";
+			while(true) {
+				ch=in.next();
+				if(isNumeric(ch)){
+					this.setValor(Integer.parseInt(ch));
+					System.out.print("Cambio realizado, siga modificando \n");
+					modificarOrden();
+				}
+				else {
+					System.out.println("Ingresa una opcion valida");
+					System.out.println(aux);
+				}
+			}
+		case "6":
+			System.out.println("Ingrese una nueva cantidad de productos en la orden :");
+			aux="Ingrese una nueva cantidad de productos en la orden :";
+			while(true) {
+				ch=in.next();
+				if(isNumeric(ch)){
+					this.setCantProductos(Integer.parseInt(ch));
+					System.out.print("Cambio realizado, siga modificando \n");
+					modificarOrden();
+				}
+				else {
+					System.out.println("Ingresa una opcion valida");
+					System.out.println(aux);
+				}
+			}
+		case "7":
+			System.out.println("Ingrese un nuevo peso total de la orden :");
+			aux="Ingrese un nuevo peso total de la orden :";
+			while(true) {
+				ch=in.next();
+				if(isNumeric(ch)){
+					this.setPesoTotal(Integer.parseInt(ch));
+					System.out.print("Cambio realizado, siga modificando \n");
+					modificarOrden();
+				}
+				else {
+					System.out.println("Ingresa una opcion valida");
+					System.out.println(aux);
+				}
+			}
+		case "8":
+			System.out.println("Ingrese un nuevo estado de la orden :");
+			this.setEstado(in.next());
+			System.out.print("Cambio realizado, siga modificando \n");
+			modificarOrden();
+		case "9":
+			Main.menuOrden();
+		}
 	}
 	public void aceptarOrden() {
 		this.estado = "En proceso";
 		System.out.println("La orden fue aceptada con exito");
-		
-	}
-	public void rechazarOrden(Orden i) {
 		
 	}
 	public void ordenEntregada() {
@@ -90,5 +196,19 @@ public class Orden {
 	}
 	float getPesoTotal() {
 		return this.pesoTotal;
+	}
+	void setEstado(String s) {
+		this.estado=s;
+	}
+	String getEstado() {
+		return this.estado;
+	}
+	private static boolean isNumeric(String cadena){
+		try {
+			Integer.parseInt(cadena);
+			return true;
+		} catch (NumberFormatException nfe){
+			return false;
+		}
 	}
 }
